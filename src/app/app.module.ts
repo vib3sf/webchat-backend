b4f65efdb5462d5d0c39667592eade6b871fb60d
vib3sf <vib3sf@proton.me>
2024-03-19 02:35:43 +0300

    remove unused stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from 'src/config/configuration';
@@ -17,7 +15,5 @@ import { ChatModule } from 'src/chat/chat.module';
     ChatModule,
     MongooseModule.forRoot('mongodb://localhost/db'),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
